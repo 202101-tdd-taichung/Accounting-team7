46d54fb657951399a714edb44ea888064abae006
Joey Chen <jay.hatelove@gmail.com>
2021-01-15 21:45:56 +0800

    [refactor] extract methods: isInvalid() and hasNoOverlap()

--- period.js
+++ period.js
@@ -8,10 +8,10 @@ export class Period {
     }
 
     overlappingDays(another) {
-        if (this.end.isBefore(this.start)) {
+        if (this.isInvalid()) {
             return 0;
         }
-        if (this.end.isBefore(another.start) || this.start.isAfter(another.end)) {
+        if (this.hasNoOverlap(another)) {
             return 0;
         }
         let overlappingStart = this.start.isAfter(another.start)
@@ -21,5 +21,13 @@ export class Period {
             ? this.end
             : another.end;
         return overlappingEnd.diff(overlappingStart, 'day') + 1;
+    }
+
+    hasNoOverlap(another) {
+        return this.end.isBefore(another.start) || this.start.isAfter(another.end);
+    }
+
+    isInvalid() {
+        return this.end.isBefore(this.start);
     }
 }
