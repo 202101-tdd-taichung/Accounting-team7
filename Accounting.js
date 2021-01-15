cb700c4422d205e865daa540510e4027aa62f580
Joey Chen <jay.hatelove@gmail.com>
2021-01-15 23:08:54 +0800

    [refactor] move method: dailyAmount() to Budget class

--- Accounting.js
+++ Accounting.js
@@ -48,7 +48,7 @@ export class Accounting {
                 const budget = budgets[start.format('YYYYMM')];
                 if (budget) {
                     const overlappingDays = budget.lastDay().diff(start, 'day') + 1;
-                    totalAmount += this.dailyAmount(budget) * overlappingDays;
+                    totalAmount += budget.dailyAmount() * overlappingDays;
                 }
             } else if (currentMonth.format('YYYYMM') === end.format('YYYYMM')) {
                 const endBudget = this.sameMonth(end, end.get('date'));
@@ -61,10 +61,6 @@ export class Accounting {
         }
 
         return totalAmount;
-    }
-
-    dailyAmount(budget) {
-        return budget.amount / budget.totalDays();
     }
 
     sameMonth(month, diffDays) {
