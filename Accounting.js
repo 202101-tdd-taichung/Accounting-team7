0b0d47e5f3550e8a145f630b393c723d14d46d34
Joey Chen <jay.hatelove@gmail.com>
2021-01-15 23:25:10 +0800

    [refactor] inline temp variables

--- Accounting.js
+++ Accounting.js
@@ -58,12 +58,10 @@ export class Accounting {
 
     overlappingDays(budget, start, end) {
         let period = new Period(start, end);
-        let start = period.start;
-        let end = period.end;
-        if (budget.yearMonth === start.format('YYYYMM')) {
-            return budget.lastDay().diff(start, 'day') + 1;
-        } else if (budget.yearMonth === end.format('YYYYMM')) {
-            return end.diff(budget.firstDay(), 'day') + 1;
+        if (budget.yearMonth === period.start.format('YYYYMM')) {
+            return budget.lastDay().diff(period.start, 'day') + 1;
+        } else if (budget.yearMonth === period.end.format('YYYYMM')) {
+            return period.end.diff(budget.firstDay(), 'day') + 1;
         } else {
             return budget.lastDay().diff(budget.firstDay(), 'day') + 1;
         }
