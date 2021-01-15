1a8b2ec345ae714acaf64d61935fc9759e2b08e5
Joey Chen <jay.hatelove@gmail.com>
2021-01-15 20:40:11 +0800

    [refactor] remove useless code

--- Accounting.js
+++ Accounting.js
@@ -54,10 +54,9 @@ export class Accounting {
                     totalAmount += firstBudget;
                 }
             } else if (currentMonth.format('YYYYMM') === end.format('YYYYMM')) {
-                // const endBudget = this.sameMonth(end, end.get('date'));
                 const budget = yearBudget[end.format('YYYYMM')];
                 if (budget) {
-                    const days = dayjs(end).daysInMonth()
+                    const days = end.daysInMonth()
                     let overlappingAmountOfLastMonth = budget.amount / days * end.get('date');
                     totalAmount += overlappingAmountOfLastMonth;
                 }
