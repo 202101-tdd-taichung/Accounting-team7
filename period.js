3849eb169113a6bd3b6e7a99ddc0773d962c785e
Joey Chen <jay.hatelove@gmail.com>
2021-01-15 23:29:59 +0800

    [refactor] clean up code

--- period.js
+++ period.js
@@ -13,11 +13,9 @@ export class Period {
         if (budget.yearMonth === this.start.format('YYYYMM')) {
             overlappingEnd = budget.lastDay();
             overlappingStart = this.start;
-            // return overlappingEnd.diff(overlappingStart, 'day') + 1;
         } else if (budget.yearMonth === this.end.format('YYYYMM')) {
             overlappingEnd = this.end;
             overlappingStart = budget.firstDay();
-            // return overlappingEnd.diff(overlappingStart, 'day') + 1;
         } else {
             overlappingEnd = budget.lastDay();
             overlappingStart = budget.firstDay();
